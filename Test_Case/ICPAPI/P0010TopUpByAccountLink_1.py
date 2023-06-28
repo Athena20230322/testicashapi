6129ff4e578d4a9bb851cb6d27dc79f6c148f60b
P10381190 <autoicash2023@gmail.com>
2023-06-28 15:32:06 +0800

    updatetesticashapi0628

---
+++ Test_Case/ICPAPI/P0010TopUpByAccountLink_1.py
@@ -0,0 +1,51 @@
+import os
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+postData = "C:\\postData\\postData32.txt"
+
+with open(postData, 'r') as f:
+    file_contents = f.read().strip().split(',')
+    enc_key_id = file_contents[0]
+    signature = file_contents[1]
+    enc_data = file_contents[2]
+
+url1 = 'https://icp-payment-stage.icashpay.com.tw/app/AccountLink/TopUpByAccountLink'
+
+headers1 = {
+    'X-ICP-EncKeyID': enc_key_id,
+    'X-iCP-Signature': signature
+}
+
+data1 = {'EncData': enc_data}
+
+response1 = requests.post(url1, headers=headers1, data=data1, verify=False)
+print(response1)
+
+response_json = response1.json()
+rtn_code = response_json['RtnCode']
+rtn_msg = response_json['RtnMsg']
+enc_text = response_json['EncData']
+
+print(f"RtnCode: {rtn_code}")
+print(f"RtnMsg: {rtn_msg}")
+
+with open("c:\\enc1.txt", 'w') as f:
+    f.write(enc_text)
+
+
+
+# Validate RtnCode value
+test_data_file = "C:\\testicashapi\\Test_Data\\ICPAPI\\P0010TopUpByAccountLink_1.txt"
+with open(test_data_file, 'r') as f:
+    file_contents = f.read()
+    expected_rtn_code = file_contents.strip().split(',')[1]
+
+if expected_rtn_code == str(rtn_code):
+    print("Test Passed")
+else:
+    print("Test Failed. Expected RtnCode: %s. Actual RtnCode: %s" % (expected_rtn_code, rtn_code))
+
+
---
+++ onekeyswitch.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+
+# 定义要执行的C#应用程序的路径
+app_paths = [
+    r'C:\GetToken-and change mobile\ConsoleApp1\bin\Debug\ConsoleApp1.exe',
+    r'C:\SendAuthSMS-and change mobile\ConsoleApp1\bin\Debug\ConsoleApp1.exe',
+    r'C:\Login-and change mobile\ConsoleApp1\bin\Debug\ConsoleApp1.exe'
+]
+
+# 依序执行每个应用程序
+for app_path in app_paths:
+    try:
+        # 使用subprocess运行C#应用程序
+        subprocess.run(app_path, check=True)
+        print(f'{app_path} 执行成功')
+
+        # 延迟5秒
+        time.sleep(5)
+    except subprocess.CalledProcessError as e:
+        print(f'{app_path} 执行失败：{e}')
